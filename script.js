98c2477118bc82d3862500e39c4280af816d4a4d
mholtyn <m.holtyn.biznes@gmail.com>
2023-05-17 17:32:07 +0200

    Add simulateRound function

--- script.js
+++ script.js
@@ -4,16 +4,51 @@
 function getComputerChoice() {
     const choices = ["Rock", "Paper", "Scissors"];
 
-    let randomInt = function(max) {
-        return Math.floor(Math.random() * max);
+    // Returns a random number from 0 to 2
+    let randomInt = function() {
+        return Math.floor(Math.random() * 3);
     }
 
-    console.log(randomInt)
+    return choices[randomInt()];
 }
 
 
-
 // TODO: Create function that plays a single round of RPS
+function simulateRound(playerChoice, computerChoice) {
+    if (playerChoice === "rock" && computerChoice === "Rock") {
+        return "Tie"
+    }
+    else if (playerChoice === "rock" && computerChoice === "Paper") {
+        return "You lose! Paper beats Rock"
+    }
+    else if (playerChoice === "rock" && computerChoice === "Scissors") {
+        return "You win! Rock beats Scissors"
+    }
+    else if (playerChoice === "paper" && computerChoice === "Paper") {
+        return "Tie"
+    }
+    else if (playerChoice === "paper" && computerChoice === "Rock") {
+        return "You win! Paper beats Rock"
+    }
+    else if (playerChoice === "paper" && computerChoice === "Scissors") {
+        return "You lose! Scissors beat Paper"
+    }
+    else if (playerChoice === "scissors" && computerChoice === "Scissors") {
+        return "Tie"
+    }
+    else if (playerChoice === "scissors" && computerChoice === "Rock") {
+        return "You lose! Rock beats Scissors"
+    }
+    else if (playerChoice === "scissors" && computerChoice === "Paper") {
+        return "You win! Scissors beat Paper"
+    }
+    else return "Error. Please try again"
+}
+
+const playerChoice = prompt("Enter your choice: ").toLowerCase();
+const computerChoice = getComputerChoice();
+
+console.log(simulateRound(playerChoice, computerChoice));
 
 
 
