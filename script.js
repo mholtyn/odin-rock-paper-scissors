d14c3d16a3a24bb96a485c609b56aefafdedcd27
mholtyn <m.holtyn.biznes@gmail.com>
2023-05-17 17:07:03 +0200

    Initial commit

---
+++ script.js
@@ -0,0 +1,20 @@
+/* Rock Paper Scissors */
+
+// TODO: Create function for computer choice
+function getComputerChoice() {
+    const choices = ["Rock", "Paper", "Scissors"];
+
+    let randomInt = function(max) {
+        return Math.floor(Math.random() * max);
+    }
+
+    console.log(randomInt)
+}
+
+
+
+// TODO: Create function that plays a single round of RPS
+
+
+
+// TODO: Create function that plays one game of RPS (5 rounds)
